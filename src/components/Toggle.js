3b164f8abe845f40ba82d1712c639899f9f672cc
tanui-ivy <ivy.tanui@student.moringaschool.com>
2025-02-21 23:13:39 +0300

    useState hook

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, {useState}from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn]= useState(false);
+
+  function handleClick(){
+    setIsOn((prevIsOn) => !prevIsOn);// toggle true/false
+  }
+
+  const colour = isOn? "red":"white";
+
+  return <button style={{background:colour}} onClick={handleClick}>
+    {isOn ? "ON" : "OFF"}
+    </button>;
 }
 
 export default Toggle;
